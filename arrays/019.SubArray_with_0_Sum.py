be31b074a7fcb3d239f8cf8a45a96d02ceab0e08
TheViralClovers <theviralclovers@gmail.com>
2023-04-29 18:35:59 +0530

    Sub array with 0 sum

---
+++ arrays/019.SubArray_with_0_Sum.py
@@ -0,0 +1,41 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to check whether there is a subarray present with 0-sum or not.
+    def subArrayExists(self,arr,n):
+        s = set()
+        sum=0
+        ##Your code here
+        #Return true or false
+        for i in arr:
+            sum+=i
+            if sum in s or sum==0:
+                return True
+            else:
+                s.add(sum)
+        return False   
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+def main():
+    T=int(input())
+    while(T>0):
+        
+        n=int(input())
+        arr=[int(x) for x in input().strip().split()]
+        if(Solution().subArrayExists(arr,n)):
+            print("Yes")
+        else:
+            print("No")
+        
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
